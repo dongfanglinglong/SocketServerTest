f74a33aeab7fce47712a87666b9b01c014061370
chenfu <chenfu@didichuxing.com>
2016-04-12 16:56:45 +0800

    增加心跳之外的数据发送

--- src/main/java/com/dongfang/server/MultiThreadServer.java
+++ src/main/java/com/dongfang/server/MultiThreadServer.java
@@ -1,11 +1,20 @@
 package com.dongfang.server;
 
+import com.dongfang.bean.BaseBean;
+import com.dongfang.bean.SocketMsgBean;
+import com.google.gson.Gson;
+import rx.Observable;
+import rx.functions.Func1;
+
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 /**
-* Created by dongfang on 2016/3/30.
-        */
+ * Created by dongfang on 2016/3/30.
+ */
 public class MultiThreadServer {
 
     public static void main(String[] args) throws Exception {
@@ -17,8 +26,31 @@ public class MultiThreadServer {
             System.out.println("服务器端等待客户端发起连接请求");
             client = server.accept();
             System.out.println("客户端向服务器端发起了连接请求,且连接成功");
-            new HandlerReadCMDInfo(client,"HANDLER CMD S2C :");
+            new HandlerReadCMDInfo(client, "HANDLER CMD S2C :");
             new HandlerReadClientInfo(client);
+            init();
+            Observable.just(client)
+                    .flatMap(new Func1<Socket, Observable<?>>() {
+                        public Observable<?> call(Socket socket) {
+                            try {
+                                outToClient = null == outToClient ? new PrintStream(socket.getOutputStream()) : outToClient;
+                                mBean.id = UI_ID + count++;
+                                mSocketMegBean.msgId = mBean.id;
+                                mBean.data = new Gson().toJson(mSocketMegBean);
+                                String str = new Gson().toJson(mBean);
+                                System.out.println("To JSON --> " + str);
+                                outToClient.println(str);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            return Observable.just(socket);
+                        }
+                    })
+                    .delay(3, TimeUnit.SECONDS)
+                    .repeat()
+                    .subscribe();
+
+
         }
 
 //        if (!flag) {
@@ -26,4 +58,26 @@ public class MultiThreadServer {
 //            new HandlerReadClientInfo(client);
 //        }
     }
+
+    static PrintStream outToClient;
+
+    static final long UI_ID = 999000;
+    static BaseBean mBean;
+    static SocketMsgBean mSocketMegBean;
+
+    static int count = 0;
+
+
+    public static void init() {
+        mBean = new BaseBean();
+        mBean.id = UI_ID;
+        mBean.msg = "ok";
+        mBean.data = "heart";
+
+        mSocketMegBean = new SocketMsgBean();
+        mSocketMegBean.msgId = UI_ID;
+        mSocketMegBean.mstType = SocketMsgBean.MSG_TYPE_SOCKET;
+    }
+
+
 }
